1a95722af01a55421e7d1f3b3a44602750f686e1
vishnukgcherupuzha <vishnukgcherupuzha@gmail.com>
2019-10-29 18:12:42 +0530

    Updates

---
+++ precache-manifest.678e3a7184fb409620de0d0fdf1bc42b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da7c752e9ffe171d1a6b69a91424130",
+    "url": "/PFMalayalam-dev-v/index.html"
+  },
+  {
+    "revision": "dd0a1f2e607950d73a8f",
+    "url": "/PFMalayalam-dev-v/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "53ae515d14971e820682",
+    "url": "/PFMalayalam-dev-v/static/css/main.a6f07f5b.chunk.css"
+  },
+  {
+    "revision": "dd0a1f2e607950d73a8f",
+    "url": "/PFMalayalam-dev-v/static/js/2.98ee6a75.chunk.js"
+  },
+  {
+    "revision": "53ae515d14971e820682",
+    "url": "/PFMalayalam-dev-v/static/js/main.898db500.chunk.js"
+  },
+  {
+    "revision": "2f72978b6eaafa6364df",
+    "url": "/PFMalayalam-dev-v/static/js/runtime-main.7fe030e4.js"
+  },
+  {
+    "revision": "4a1d362758ce64bd2d8a043e7f06b246",
+    "url": "/PFMalayalam-dev-v/static/media/image1.4a1d3627.png"
+  },
+  {
+    "revision": "2707861417ea381298159b8973704f44",
+    "url": "/PFMalayalam-dev-v/static/media/image2.27078614.jpg"
+  },
+  {
+    "revision": "e53924b2528088a60dc18ec1eb3cab6e",
+    "url": "/PFMalayalam-dev-v/static/media/image3.e53924b2.jpg"
+  },
+  {
+    "revision": "2a5be244afe94dcc837e360c9ba51380",
+    "url": "/PFMalayalam-dev-v/static/media/opps.2a5be244.jpg"
+  }
+]);
